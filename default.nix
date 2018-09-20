730b812ea816a754cd702bccef47d37c454792b7
Rupert Horlick <rupert.horlick@iohk.io>
2018-09-20 18:19:00 -0400

    Update default.nix to use forked snack

--- default.nix
+++ default.nix
@@ -2,27 +2,15 @@ let
 
 snack-src = (import <nixpkgs> {}).fetchFromGitHub {
   repo = "snack";
-  owner = "nmattia";
-  rev = "97d41baf2b0a2696079852e3b7ad38783f04d9bd";
-  sha256 = "1kyx9bpmn8s1axiaxbvqgxbjvzs1y71iilaz19iycvf3bqyx74fd";
+  owner = "ruhatch";
+  rev = "5f41965d12eadf53403162e930e5c1387890c67f";
+  sha256 = "10hv2an6g9zhfjwg86p97vbm27b123l0fc5x8xqj278dmlg79035";
 };
 
 in
 
 { pkgs ? import (import "${snack-src}/nix/nixpkgs") {}
-, snack ? import
-  ("${snack-src}/snack-lib")
-  { lib = pkgs.lib;
-    haskellPackages = pkgs.haskellPackages;
-    makeWrapper = pkgs.makeWrapper;
-    rsync = pkgs.rsync;
-    stdenv = pkgs.stdenv;
-    symlinkJoin = pkgs.symlinkJoin;
-    writeScriptBin = pkgs.writeScriptBin;
-    writeText = pkgs.writeText;
-    runCommand = pkgs.runCommand;
-    callPackage = pkgs.callPackage;
-  }
+, snack ? import "${snack-src}/snack-lib" { inherit pkgs; }
 }:
 
-snack.inferSnackBuild ./snack.nix
+snack.mkPackage (import ./snack.nix)
